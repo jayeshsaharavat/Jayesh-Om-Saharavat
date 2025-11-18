649f334448097e0044a52aa537a55f0d9a0a5245
JAYESH OM SAHARAVAT <jayeshsaharavat1@gmail.com>
2025-11-18 23:43:04 +0530

    Add files via upload

---
+++ 100 days of C/day 62/Q112.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[100];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int max_sum = arr[0];     // to store the maximum sum found so far
+    int current_sum = arr[0]; // current running sum
+
+    for (int i = 1; i < n; i++) {
+        // either extend the previous subarray or start a new one
+        if (current_sum + arr[i] > arr[i])
+            current_sum = current_sum + arr[i];
+        else
+            current_sum = arr[i];
+
+        // update max_sum if current_sum is greater
+        if (current_sum > max_sum)
+            max_sum = current_sum;
+    }
+
+    printf("Maximum subarray sum = %d\n", max_sum);
+    return 0;
+}
+
