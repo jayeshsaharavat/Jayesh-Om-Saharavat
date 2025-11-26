2202261e329676690223d99fa6037c538d5a8b7e
JAYESH OM SAHARAVAT <jayeshsaharavat1@gmail.com>
2025-11-26 23:46:50 +0530

    Add gender input handling in Person struct

---
+++ 100 days of C/day 90/140.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+enum Gender {
+    MALE,
+    FEMALE
+};
+
+struct Person {
+    enum Gender gender;
+};
+
+int main() {
+    struct Person p;
+    char input[20];
+
+    printf("Gender=");
+    scanf("%s", input);
+
+    if (strcmp(input, "MALE") == 0) {
+        p.gender = MALE;
+    } else if (strcmp(input, "FEMALE") == 0) {
+        p.gender = FEMALE;
+    } else {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    if (p.gender == MALE) {
+        printf("Male");
+    } else {
+        printf("Female");
+    }
+
+    return 0;
+}
