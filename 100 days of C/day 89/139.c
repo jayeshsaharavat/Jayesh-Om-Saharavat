3eaf36858cf98364bdcb580e810fcc456fa77300
JAYESH OM SAHARAVAT <jayeshsaharavat1@gmail.com>
2025-11-26 23:45:41 +0530

    Add enum for status values in 139.c

---
+++ 100 days of C/day 89/139.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+enum Values{
+    SUCCESS,
+    FAILURE,
+    TIMEOUT
+};
+
+int main() {
+    printf("SUCCESS=%d, ",SUCCESS);
+    printf("FAILURE=%d, ", FAILURE);
+    printf("TIMEOUT=%d\n", TIMEOUT);
+
+    return 0;
+}
