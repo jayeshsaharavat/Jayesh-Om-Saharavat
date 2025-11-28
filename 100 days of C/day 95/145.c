ca523f6f08542a4006532f2d40e3f056aea31111
JAYESH OM SAHARAVAT <jayeshsaharavat1@gmail.com>
2025-11-28 23:22:23 +0530

    Add files via upload

---
+++ 100 days of C/day 95/145.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef struct Student_Record {
+    char name[100];
+    int roll_no;
+    int marks;
+} student;
+int main() {
+    student s[3];
+    
+    printf ("3 students: ");
+    for (int i=0 ; i<3 ; i++) {
+        scanf("%s", s[i].name);
+        scanf("%d", &s[i].roll_no);
+        scanf("%d", &s[i].marks);
+    }
+    int max_marks = s[0].marks;
+    int j = 0;
+
+    for (int i=1; i<3 ; i++) {
+        if (s[i].marks > max_marks) {
+            max_marks = s[i].marks;
+            j=i;
+        }
+    }
+    printf("Top Student: %s | Roll: %d | Marks: %d",s[j].name, s[j].roll_no, s[j].marks);
+    
+    return 0;
+}
+
